4a1383447b60b374aa2ca31e07592c1cb4d8994a
dgcole55 <32987982+dgcole55@users.noreply.github.com>
2025-02-12 09:18:22 -0500

    modified:   build/doctrees/environment.pickle 	modified:   build/doctrees/index.doctree 	modified:   build/html/appendices/Homework.html 	modified:   build/html/appendices/appendix-010.html 	modified:   build/html/copyright/copyright.html 	modified:   build/html/genindex.html 	modified:   build/html/index.html 	modified:   build/html/objects.inv 	modified:   build/html/search.html 	modified:   build/html/searchindex.js 	modified:   build/html/syllabus/academic-policies.html 	modified:   build/html/syllabus/schedule.html 	modified:   build/html/syllabus/syllabus.html 	modified:   build/html/topics/topic-005.html 	modified:   build/html/topics/topic-007.html 	modified:   build/html/topics/topic-010.html 	modified:   build/html/topics/topic-020.html 	modified:   build/html/topics/topic-040.html 	modified:   source/conf.py 	modified:   source/topics/part3.rst 	modified:   source/topics/part4.rst 	renamed:    source/topics/topic-060.rst -> source/topics/xtopic-060.rst

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,7 @@ release = '1.0-beta'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [   'sphinxcontrib.ansi']
 
 templates_path = ['_templates']
 exclude_patterns = []
